8514eff0fa8f864f1574508aa8f83f3b81c870e3
yasutake-yasushi <yasutake@yas-ltd.com>
2026-04-18 17:45:13 +0900

    chore: remove sql DDL directory (EF Core migrations are used instead)

--- PSWPService/sql/sqlite/07_system_settings.sql
+++
@@ -1,9 +0,0 @@
--- ============================================================
--- Table: SystemSettings  (singleton: Id = 1)
--- ============================================================
-CREATE TABLE IF NOT EXISTS "SystemSettings" (
-    "Id"             INTEGER NOT NULL CONSTRAINT "PK_SystemSettings" PRIMARY KEY AUTOINCREMENT,
-    "MipsFilePath"   TEXT NOT NULL,
-    "StrikeFilePath" TEXT NOT NULL,
-    "UpdatedAt"      TEXT NOT NULL
-);
